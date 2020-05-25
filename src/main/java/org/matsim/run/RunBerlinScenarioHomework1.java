3a19a43aefe47dc75c604fa86732016dd82405b9
altitudeflight <erik.stiegemann@me.com>
2020-05-25 19:55:06 +0200

    added comment regarding position of config arguments

--- src/main/java/org/matsim/run/RunBerlinScenarioHomework1.java
+++ src/main/java/org/matsim/run/RunBerlinScenarioHomework1.java
@@ -79,6 +79,7 @@ public final class RunBerlinScenarioHomework1 {
 		Scenario scenario = prepareScenario( config ) ;
 		Controler controler = prepareControler( scenario ) ;
 
+		//in the lab Dominik did this down in the prepareControler method, but it's working from here and might be easier to access
 		config.controler().setLastIteration(1); //Erik: changed last iteration to 1
 		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists); //Erik: delete output directory if existent
 
