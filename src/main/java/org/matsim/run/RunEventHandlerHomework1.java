62ac5abfdd507b2f70a3ddd5cb0f8cf4b26789a4
stepperstep <stefan.schulz.94@gmail.com>
2020-06-04 10:28:23 +0200

    RunEventHandler und LinkEventHandler erstellt. - check - Testdateien mit Testlink funktioniert. - check - linksToAnalyze-Datei erstellt. - in progress - Als nächstes Buffered Reader für linksToAnalyze. - waits -

--- src/main/java/org/matsim/run/RunEventHandlerHomework1.java
+++ src/main/java/org/matsim/run/RunEventHandlerHomework1.java
@@ -4,20 +4,23 @@ import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
 
-public class RunEventHandler {
+public class RunEventHandlerHomework1 {
 
     public static void main(String[] args) {
 
-        String inputFile = "scenarios/berlin-v5.5-1pct/output-berlin-v5.5-1pct/ITERS/it.0/berlin-5.5-1pct.0.events.xml.gz";
+        String eventInputFile = "scenarios/berlin-v5.5-1pct/output-berlin-v5.5-1pct/ITERS/it.0/berlin-v5.5-1pct.0.events.xml.gz";
+        String linkInputFile = "";
         String outputFile = "scenarios/berlin-v5.5-1pct/output-berlin-v5.5-1pct/ITERS/testHandledEvents.txt";
 
         EventsManager eventsManager = EventsUtils.createEventsManager();
 
-        MatsimEventsReader eventsReader = new MatsimEventsReader(eventsManager);
-        eventsReader.readFile(inputFile);
-
-        LinkEventHandler linkEventHandler = new LinkEventHandler(outputFile);
+        LinkEventHandlerHomework1 linkEventHandler = new LinkEventHandlerHomework1(outputFile);
         eventsManager.addHandler(linkEventHandler);
+
+        MatsimEventsReader eventsReader = new MatsimEventsReader(eventsManager);
+        eventsReader.readFile(eventInputFile);
+
+        linkEventHandler.printResult();
 
 
     }
