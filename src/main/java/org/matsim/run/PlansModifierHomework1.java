c69a9a52edc8224a4dba29c0fb59296e8c9ee24c
altitudeflight <erik.stiegemann@me.com>
2020-06-06 10:22:22 +0200

    more ideas for PlansModifierHomework1

--- src/main/java/org/matsim/run/PlansModifierHomework1.java
+++ src/main/java/org/matsim/run/PlansModifierHomework1.java
@@ -1,10 +1,12 @@
 package org.matsim.run;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.PopulationWriter;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.population.io.PopulationReader;
 import org.matsim.core.scenario.ScenarioUtils;
+import scala.collection.script.Remove;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -51,6 +53,27 @@ public class PlansModifierHomework1 {
         }
 
         //modify plans with this array here, no idea yet...
+
+        for(String linkId : RemovedLinks) {
+            scenario.getPopulation().getPersons().values().parallelStream()
+                    .flatMap(person -> person.getPlans().stream())
+                    .flatMap(plan -> plan.getPlanElements().stream())
+                    .filter(element -> element instanceof Leg)
+                    .map(element -> (Leg) element)
+                    .filter(leg -> leg.getRoute().getRouteType().
+                    .filter()
+
+                            //Routentyp checken
+                            //casten zu diesem Routentyp
+                            //prüfen ob es meine Links enthält
+
+                            //.getRouteDescription()
+                            //.contains(linkId))
+                  //  .forEach(leg -> leg.setRoute(null));
+        }
+
+        System.out.println(scenario.getPopulation().getPersons());
+
 
         PopulationWriter populationWriter = new PopulationWriter(scenario.getPopulation());
         populationWriter.write(outputPlans.toString());
